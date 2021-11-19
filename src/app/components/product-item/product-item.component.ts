67f0d81e7374e30343ec375b877b2691b9b9da24
Andrea Knez Karačić <zelenokosazelena@gmail.com>
2021-11-19 15:21:08 +0100

    feat: routing to product details

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from 'src/app/model/product';
 
 @Component({
@@ -9,7 +10,11 @@ import { Product } from 'src/app/model/product';
 export class ProductItemComponent implements OnInit {
   @Input() product!: Product;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  openDetails(): void {
+    this.router.navigate(['products/', this.product.id]);
+  }
 }
